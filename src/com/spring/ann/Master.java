2d7b1109b0a1abe4ccbd651210594613c1986891
Ayham <ayham.ayham909>
2022-08-29 09:25:28 +0300

    spring Autowired Function and field

--- src/com/spring/ann/Master.java
+++ src/com/spring/ann/Master.java
@@ -5,25 +5,35 @@ import org.springframework.stereotype.Component;
 
 @Component("master")
 public class Master implements MasterInterface{
-
+    
+	@Autowired
 	private Sportinterface sportinterface;
 	
 	
-	
+	/*
 	@Autowired
 	public Master(Sportinterface sportinterface) {
 		this.sportinterface = sportinterface;
 			
 	}
+	*/
 	
 	
+	/*
+	@Autowired
+	public void setSportinterface(Sportinterface sportinterface) {
+		this.sportinterface = sportinterface;
+	}
+	*/
 	
-
+	
 	@Override
 	public void name() {
 		System.out.println("i am Master");
 
 	}
+	
+	
 
 	@Override
 	public void callMysport() {
